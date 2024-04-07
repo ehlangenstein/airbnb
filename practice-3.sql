90c85049652d35c1686415a8c2232d488cdabe42
Elizabeth Langenstein <elizabeth.langenstein@kellogg.northwestern.edu>
2024-04-07 23:06:32 +0000

    Completed Practice Assignment 1

--- practice-3.sql
+++ practice-3.sql
@@ -1,5 +1,7 @@
 -- What are all the neighborhoods, alphabetically?
-
+select distinct neighborhood
+from listings
+order by neighborhood;
 -- +------------------------+
 -- | Albany Park            |
 -- | Archer Heights         |
--- practice-6.sql
+++ practice-6.sql
@@ -1,5 +1,8 @@
 -- How many reviews are written per neighborhood?
-
+SELECT neighborhood, count(*) 
+from reviews
+inner join listings on listings.id = reviews.listing_id 
+group by listings.neighborhood;
 -- +------------------------+----------+
 -- | Albany Park            | 2557     |
 -- | Archer Heights         | 541      |
